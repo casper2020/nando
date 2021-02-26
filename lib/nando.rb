fc9a66c80ca9ef8d2891395730a8fc0e43e1c011
Fernando Alves <fernando.alves@cldware.com>
2021-02-26 13:58:14 +0000

    Small reestructure to clean up the code

--- lib/nando.rb
+++ lib/nando.rb
@@ -1,10 +1,3 @@
+require 'nando'
 require "nando/version"
-
-module Nando
-  class Error < StandardError; end
-  
-  def self.new_migration
-    puts 'Creating a new migration'
-  end
-
-end
+require 'nando/generator'
---
+++ lib/nando/generator.rb
@@ -0,0 +1,7 @@
+def self.new_migration
+  puts 'Creating a new migration'
+end
+
+# module Nando
+#   class Error < StandardError; end
+# end
