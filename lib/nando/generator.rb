864d562127c76a5def795c05ef95cd34a0cb4118
Fernando Alves <fernando.alves@cldware.com>
2021-02-27 19:07:17 +0000

    Added method to convert a migration name to the correct format

--- lib/nando/generator.rb
+++ lib/nando/generator.rb
@@ -1,6 +1,7 @@
 def self.new_migration (args = {})
-  migration_name = args.fetch(:name)
-  puts "Creating a new migration: #{args[:name]}"
+  migration_name = args.fetch(:name).underscore
+
+  puts "Creating a new migration: #{migration_name}"
 end
 
 # timestamp example: 20200713100600
@@ -8,3 +9,14 @@ end
 # module Nando
 #   class Error < StandardError; end
 # end
+
+class String
+  # used to convert to snake case
+  def underscore
+    self.gsub(/::/, '/')
+        .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+        .gsub(/([a-z\d])([A-Z])/,'\1_\2')
+        .tr("-", "_")
+        .downcase
+  end
+end
