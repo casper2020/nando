53d1bfe6a19247bb0aa6191161eef7886fc1f226
Fernando Alves <fernando.alves@cldware.com>
2021-02-27 22:19:51 +0000

    Generate now creates a file in a given dir

--- lib/nando/generator.rb
+++ lib/nando/generator.rb
@@ -7,7 +7,24 @@ module NandoMigration
   
     migration_file_name = "#{migration_timestamp}_#{migration_name}"
   
+    dir = 'db/migrate' # this might change later
+    path = "#{dir}/#{migration_file_name}"
+
+    create_migration_file(path)
+
     puts "Creating a new migration: #{migration_file_name}"
+  end
+
+  def self.create_migration_file (filepath)
+    dir = File.dirname(filepath)
+
+    if !File.directory?(dir)
+      STDERR.puts %Q[No directory "#{dir}" was found.]
+      exit 3
+    end
+    
+    filepath << ".rb" # append the rb extension
+    File.new(filepath, 'w')
   end
 end
 
