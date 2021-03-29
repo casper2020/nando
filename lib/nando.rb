e702176640b84b21b24c7c5b51d9828ef373c557
Fernando Alves <fernando.alves@cldware.com>
2021-03-29 15:58:39 +0100

    Reverted changes on utils, need to review structure later no

--- lib/nando.rb
+++ lib/nando.rb
@@ -1,5 +1,5 @@
 require 'nando'
-require "nando/version"
+require 'nando/version'
 require 'nando/migrator'
 require 'nando/migration'
 require 'nando/generator'
--- lib/nando/utils.rb
+++ lib/nando/utils.rb
@@ -1,43 +1,6 @@
 module NandoUtils
 
-  def self.get_migration_files (directory)
-    files = Dir.children(directory)
+  # TODO: move helper methods here, to not fill the main files
 
-    migration_files = []
-    for filename in files do
-      if !/\d\_.*\.rb$/.match(filename)
-        puts "Warning: #{filename} does not have a valid migration name"
-        next
-      end
-
-      migration_files.push(filename)
-    end
-
-    migration_files.sort! # sort to ensure the migrations are executed chronologically
-  end
-
-  # TODO: might merge with "get_migration_files"
-  def self.get_migration_files_to_rollback (directory, versions_to_rollback)
-    files = Dir.children(directory)
-
-    migration_files = []
-    for filename in files do
-      match = /(\d+)\_.*\.rb$/.match(filename)
-      if match[0].nil?
-        # TODO: test this again for rollback
-        puts "Warning: #{filename} does not have a valid migration name"
-        next
-      end
-
-      if versions_to_rollback.include?(match[1])
-        migration_files.push(filename)
-      end
-    end
-
-    migration_files.sort.reverse # sort and reverse to ensure the migrations are executed chronologically (backwards)
-  end
-
-
-  
 end
 
