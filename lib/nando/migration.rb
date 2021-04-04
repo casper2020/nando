c2d48e65ca32f2672990b6d5ca8ff99279bbb2e0
FernandoJAlves <fernando.alves@cldware.com>
2021-04-04 01:28:52 +0100

    Added search for respective DOWN directives, missing inserting annotation and updating it

--- lib/nando/migration.rb
+++ lib/nando/migration.rb
@@ -11,6 +11,12 @@ module Nando
       @db_connection.exec(sql)
     end
 
+    # TODO: any better place to put this method?
+    def update_function (sql)
+      # TODO: add validations here
+      @db_connection.exec(sql)
+    end
+
     def execute_migration (method)
       # TODO: review this is the best way of creating a transaction (don't know if re-assigning connections has weird behaviours)
       old_connection = @db_connection
--- lib/nando/updater.rb
+++ lib/nando/updater.rb
@@ -8,11 +8,12 @@ module MigrationUpdater
 
     @working_directory = working_directory
     @lines = File.readlines(migration_file_path)
-    @trigger_keyword = 'NANDO'
-    @trigger = "(\s*)(?:#\s(?:#{@trigger_keyword}:)(?:\s)?)(.*)" # match 1 is the space to indent, and match 2 is the file being linked
+    up_keyword = 'NANDO'
+    @up_annotation_trigger = "(\s*)(?:#\s(?:#{up_keyword}:)(?:\s)?)(.*)" # match 1 is the space to indent, and match 2 is the file being linked
     @last_scanned_index = 0
 
     @changed_file = false
+    @source_files_copied = []
 
     find_and_update()
 
@@ -33,13 +34,25 @@ module MigrationUpdater
     execute_match = nil
     ending_execute_match = nil
 
+    annotation_file = nil
+    duplicate_annotation = false
+
     @lines.each_with_index do |line, line_index|
-      line_match = line.match(@trigger)
+      line_match = line.match(@up_annotation_trigger)
 
       # found a annotation that has not been updated
       if !line_match.nil? && line_index > @last_scanned_index
         @last_scanned_index = line_index
         do_another_loop = true
+        annotation_file = line_match[2]
+
+        if @source_files_copied.include?(annotation_file)
+          _warn "The file '#{annotation_file}' has already been updated in the current migration. Skipping!"
+          duplicate_annotation = true
+          break
+        else
+          @source_files_copied.push(annotation_file)
+        end
 
         # find beginning of block
         if execute_match = @lines[line_index+1].match("(.*)update_function(.*)SQL(.*)\n")
@@ -64,8 +77,9 @@ module MigrationUpdater
     end
 
     if do_another_loop
-      unless starting_sql_index.nil? && ending_sql_index.nil?
-        curr_source_file = "#{@working_directory}/#{line_match[2]}"
+      # update the block for the current annotation (if not a duplicate)
+      if !(starting_sql_index.nil? && ending_sql_index.nil?) && !duplicate_annotation
+        curr_source_file = "#{@working_directory}/#{annotation_file}"
 
         if File.file?(curr_source_file)
           # delete from array lines for current update_function block (if there is any)
@@ -87,16 +101,103 @@ module MigrationUpdater
           @lines.insert(starting_sql_index, *curr_file_lines)
 
           # TODO: create/update the equivalent DOWN => have a similiar directive in the down method, with the path as well, to allow for "easy" matching (?)
+          find_and_update_respective_down_directive(annotation_file)
 
           @last_scanned_index = starting_sql_index + curr_file_lines.length - 1
           @changed_file = true
           _success "Updated content for #{curr_source_file}"
         else
-          _warn "Couldn't find file: #{curr_source_file} => Skipping that one!"
+          _warn "Couldn't find file: '#{curr_source_file}' => Skipping that one!"
         end
       end
       find_and_update()
     end
+
+  end
+
+  def self.find_and_update_respective_down_directive (source_file_path)
+    # if a NANDO directive is being updated, then we need to find the respetive down (X)
+    # start from the top of the file, try and find the directive (may try to optmize this later, to start at "def down") (X)
+    # if the directive is found, update it.
+    # if not, create one at the bottom of the file and update it
+    # matching is done using the source_file_path, but the code to fill the down comes from previous migrations (NOT THE FILE)
+
+    down_keyword = 'NANDO_DOWN'
+    down_annotation_index = nil
+    down_annotation_trigger = "(\s*)(?:#\s(?:#{down_keyword}:)(?:\s)?)(?:#{source_file_path})" # match 1 is the space to indent
+
+    down_method_index = nil
+    down_method_trigger = "(\s*)def(?:\s*)down(.*)"
+    down_method_end_trigger = nil # to find respective "end"
+
+    line_match = nil
+
+    # find down annotation
+    @lines.each_with_index do |line, line_index|
+      # find start of down method (ignore before that)
+      if down_method_index.nil?
+        line_match = line.match(down_method_trigger)
+        if !line_match.nil?
+          _debug 'Found down method'
+          down_method_index = line_index
+          down_method_indent = line_match[1]
+          down_method_end_trigger = "^(?:#{line_match[1]}end).*"
+        end
+        next
+      end
+
+      # start looking for an annotation
+      line_match = line.match(down_annotation_trigger)
+
+      # found a annotation that has not been updated
+      if !line_match.nil?
+        _debug 'FOUND MATCH'
+        break
+        down_annotation_index = line_index
+        # find beginning of block
+        if execute_match = @lines[line_index+1].match("(.*)update_function(.*)SQL(.*)\n")
+          starting_sql_index = line_index + 1
+          ending_trigger = execute_match[1] + 'SQL' + "\n"
+
+          # find ending of block
+          for ending_block_index in line_index+2..@lines.length-1 do
+            if ending_execute_match = @lines[ending_block_index].match(ending_trigger)
+              ending_sql_index = ending_block_index
+              break
+            end
+          end
+        # we need to create an update_function block, since one does not exist
+        else
+          starting_sql_index = line_index + 1
+          ending_sql_index = starting_sql_index - 1
+          prepend_append_execute = true
+        end
+        break
+      end
+    end
+
+    # no annotation found, create one
+    if down_annotation_index.nil?
+      # create annotation
+      _debug 'DID NOT FIND MATCH'
+
+      @lines.each_with_index do |line, line_index|
+        # ignore before "def down"
+        if line_index <= down_method_index
+          next
+        end
+
+        # look for the "end" of "def down"
+        line_match = line.match(down_method_end_trigger)
+        if !line_match.nil?
+          _debug "FOUND END OF DOWN: #{line_index}"
+
+          # TODO: insert annotation here
+        end
+      end
+    end
+
+    # update annotation - TODO
 
   end
 
