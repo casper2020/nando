72e8d5169a87469f23899dc599d2e7f8721c8fe2
Fernando Alves <fernando.alves@cldware.com>
2021-12-10 12:52:41 +0000

    Added first test for Nando, checking the database URL parsing

--- spec/nando_spec.rb
+++ spec/nando_spec.rb
@@ -3,11 +3,33 @@ RSpec.describe Nando do
     expect(Nando::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(false)
+  it "handles database URLs" do
+    # handle normal URLs
+    match = NandoMigrator.instance.parse_database_url('postgres://toconline:toconline@127.0.0.1:5432/toconline')
+    expect(match[1]).to eq('postgres')
+    expect(match[2]).to eq('toconline')
+    expect(match[3]).to eq('toconline')
+    expect(match[4]).to eq('127.0.0.1')
+    expect(match[5]).to eq('5432')
+    expect(match[6]).to eq('toconline')
+
+    # handle URLs with "-"
+    match = NandoMigrator.instance.parse_database_url('postgres://toconline-1:toconline-2@localhost:123/nando-test-migrate')
+    expect(match[1]).to eq('postgres')
+    expect(match[2]).to eq('toconline-1')
+    expect(match[3]).to eq('toconline-2')
+    expect(match[4]).to eq('localhost')
+    expect(match[5]).to eq('123')
+    expect(match[6]).to eq('nando-test-migrate')
+
+    # handle URLs with "_"
+    match = NandoMigrator.instance.parse_database_url('postgres://toconline_1:toconline_2@host-name_test:8080/nando_test_migrate')
+    expect(match[1]).to eq('postgres')
+    expect(match[2]).to eq('toconline_1')
+    expect(match[3]).to eq('toconline_2')
+    expect(match[4]).to eq('host-name_test')
+    expect(match[5]).to eq('8080')
+    expect(match[6]).to eq('nando_test_migrate')
   end
 
-  it "has a migrate command" do
-    expect(Foodie::Food.pluralize("Tomato")).to eql("Tomatoes")
-  end
 end
